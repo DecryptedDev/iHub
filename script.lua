876307f2ecc4d13d71bdf4ce78b83bbc34fc1001
iHubDev <88538699+DecryptedDev@users.noreply.github.com>
2021-08-08 23:29:39 +0530

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,9 @@
 if game.Placeid == 3956818381 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/DecryptedDev/iHub/main/Games/NinjaLegends.lua"), true)
 end     
+if game.PlaceId == 1962086868 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/DecryptedDev/iHub/main/Games/TowerOfHell.lua"))()
+end   
+if game.PlaceId == 286090429 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/DecryptedDev/iHub/main/Games/Arsenal.lua"))()
+end   
