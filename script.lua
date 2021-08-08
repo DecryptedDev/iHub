8d9d4abd43b23d8434fd8e78394453768fdb0ff4
iHubDev <88538699+DecryptedDev@users.noreply.github.com>
2021-08-08 23:37:48 +0530

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,12 +4,3 @@ end
 if game.PlaceId == 1962086868 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/DecryptedDev/iHub/main/Games/TowerOfHell.lua"))()
 end   
-if game.PlaceId == 286090429 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/DecryptedDev/iHub/main/Games/Arsenal.lua"))()
-else
-   local discordinvite = "https://discord.gg/a5gBaqB8uS"
-   wait(1)
-   game.Players.LocalPlayer:Kick("iHub Game Bot has detected that the game you have executed iHub on is not supported, The discord server has been copied to the clipboard for you user to see the supported games, Thank you -Decrypted")
-   wait(1)
-   setclipboard(discordinvite)
-end   
